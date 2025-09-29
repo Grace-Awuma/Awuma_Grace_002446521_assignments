55244f926b114c0774ea87bef88e574fba1b5cdf
Grace-Awuma <awuma.g@northeastern.edu>
2025-09-29 14:46:33 -0400

    Manager functionalitoes implemented

--- Coffee Shop POS/src/ui/manager/ManagerWorkAreaJPanel.java
+++ Coffee Shop POS/src/ui/manager/ManagerWorkAreaJPanel.java
@@ -4,17 +4,24 @@
  */
 package ui.manager;
 
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+import model.Business;
+
 /**
  *
  * @author grace
  */
 public class ManagerWorkAreaJPanel extends javax.swing.JPanel {
-
+    private JPanel mainWorkArea;  // ADD THIS
+    private Business business;
     /**
      * Creates new form AddProductJPanel
      */
-    public ManagerWorkAreaJPanel() {
+    public ManagerWorkAreaJPanel(JPanel mainWorkArea, Business business) {
         initComponents();
+        this.mainWorkArea = mainWorkArea;  // ADD THIS
+        this.business = business;  
     }
 
     /**
@@ -26,100 +33,181 @@ public class ManagerWorkAreaJPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jSplitPane1 = new javax.swing.JSplitPane();
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton6 = new javax.swing.JButton();
-        jButton7 = new javax.swing.JButton();
-        jPanel2 = new javax.swing.JPanel();
+        splitPane = new javax.swing.JSplitPane();
+        menuBar = new javax.swing.JPanel();
+        lblWelcome = new javax.swing.JLabel();
+        btnAddCustomer = new javax.swing.JButton();
+        btnListOrders = new javax.swing.JButton();
+        btnManageProduct = new javax.swing.JButton();
+        btnSearchCustomer = new javax.swing.JButton();
+        btnViewProduct = new javax.swing.JButton();
+        btnHome = new javax.swing.JButton();
+        workArea = new javax.swing.JPanel();
 
-        setLayout(new java.awt.BorderLayout());
+        splitPane.setOrientation(javax.swing.JSplitPane.VERTICAL_SPLIT);
 
-        jSplitPane1.setOrientation(javax.swing.JSplitPane.VERTICAL_SPLIT);
+        lblWelcome.setText("<WelcomeMsg>");
 
-        jLabel1.setText("<Welcome Msg>");
+        btnAddCustomer.setText("Add Customer + Order");
+        btnAddCustomer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddCustomerActionPerformed(evt);
+            }
+        });
 
-        jButton1.setText("jButton1");
+        btnListOrders.setText("List Orders");
+        btnListOrders.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnListOrdersActionPerformed(evt);
+            }
+        });
 
-        jButton2.setText("jButton2");
+        btnManageProduct.setText("Manage Products");
+        btnManageProduct.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnManageProductActionPerformed(evt);
+            }
+        });
 
-        jButton3.setText("jButton3");
+        btnSearchCustomer.setText("Search Customers");
+        btnSearchCustomer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSearchCustomerActionPerformed(evt);
+            }
+        });
 
-        jButton4.setText("jButton4");
+        btnViewProduct.setText("View Product");
+        btnViewProduct.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnViewProductActionPerformed(evt);
+            }
+        });
 
-        jButton6.setText("jButton6");
+        btnHome.setText("Home");
+        btnHome.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnHomeActionPerformed(evt);
+            }
+        });
 
-        jButton7.setText("jButton7");
+        javax.swing.GroupLayout menuBarLayout = new javax.swing.GroupLayout(menuBar);
+        menuBar.setLayout(menuBarLayout);
+        menuBarLayout.setHorizontalGroup(
+            menuBarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(menuBarLayout.createSequentialGroup()
+                .addGap(17, 17, 17)
+                .addComponent(lblWelcome)
+                .addGap(50, 50, 50)
+                .addComponent(btnAddCustomer)
+                .addGap(18, 18, 18)
+                .addComponent(btnListOrders)
+                .addGap(18, 18, 18)
+                .addComponent(btnManageProduct)
+                .addGap(18, 18, 18)
+                .addComponent(btnSearchCustomer)
+                .addGap(26, 26, 26)
+                .addComponent(btnViewProduct)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
+                .addComponent(btnHome))
+        );
+        menuBarLayout.setVerticalGroup(
+            menuBarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(menuBarLayout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addGroup(menuBarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblWelcome)
+                    .addComponent(btnAddCustomer)
+                    .addComponent(btnListOrders)
+                    .addComponent(btnManageProduct)
+                    .addComponent(btnSearchCustomer)
+                    .addComponent(btnViewProduct)
+                    .addComponent(btnHome))
+                .addContainerGap(40, Short.MAX_VALUE))
+        );
+
+        splitPane.setTopComponent(menuBar);
+
+        workArea.setLayout(new java.awt.CardLayout());
+        splitPane.setRightComponent(workArea);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(36, 36, 36)
-                .addComponent(jLabel1)
-                .addGap(54, 54, 54)
-                .addComponent(jButton1)
-                .addGap(18, 18, 18)
-                .addComponent(jButton2)
-                .addGap(18, 18, 18)
-                .addComponent(jButton3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton7)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton6)
-                .addContainerGap())
+            .addComponent(splitPane)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(26, 26, 26)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3)
-                    .addComponent(jButton4)
-                    .addComponent(jButton6)
-                    .addComponent(jButton7))
-                .addContainerGap(12, Short.MAX_VALUE))
+            .addComponent(splitPane)
         );
 
-        jSplitPane1.setTopComponent(jPanel1);
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 704, Short.MAX_VALUE)
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 423, Short.MAX_VALUE)
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-
-        jSplitPane1.setRightComponent(jPanel2);
-
-        add(jSplitPane1, java.awt.BorderLayout.PAGE_START);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnViewProductActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnViewProductActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnViewProductActionPerformed
+
+    private void btnAddCustomerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddCustomerActionPerformed
+        // TODO add your handling code here:
+         AddCustomerOrderJPanel panel = new AddCustomerOrderJPanel(workArea, business);
+        workArea.add("AddCustomerOrder", panel);
+        CardLayout layout = (CardLayout) workArea.getLayout();
+        layout.next(workArea);
+    }//GEN-LAST:event_btnAddCustomerActionPerformed
+
+    private void btnManageProductActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageProductActionPerformed
+        // TODO add your handling code here:
+        ManageProductJPanel panel = new ManageProductJPanel(workArea, business);
+        workArea.add("ManageProduct", panel);
+        CardLayout layout = (CardLayout) workArea.getLayout();
+        layout.next(workArea);
+    }//GEN-LAST:event_btnManageProductActionPerformed
+
+    private void btnSearchCustomerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSearchCustomerActionPerformed
+        // TODO add your handling code here:
+        SearchCustomerJPanel panel = new SearchCustomerJPanel(workArea, business);
+        workArea.add("SearchCustomer", panel);
+        CardLayout layout = (CardLayout) workArea.getLayout();
+        layout.next(workArea);
+    }//GEN-LAST:event_btnSearchCustomerActionPerformed
+
+    private void btnListOrdersActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnListOrdersActionPerformed
+        // TODO add your handling code here:
+        ListOrdersJPanel panel = new ListOrdersJPanel(workArea, business);
+        workArea.add("ListOrders", panel);
+        CardLayout layout = (CardLayout) workArea.getLayout();
+        layout.next(workArea);
+    }//GEN-LAST:event_btnListOrdersActionPerformed
+
+    private void btnHomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHomeActionPerformed
+        // TODO add your handling code here:
+         CardLayout layout = (CardLayout) mainWorkArea.getLayout();
+        layout.show(mainWorkArea, "Home");
+    }//GEN-LAST:event_btnHomeActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton6;
-    private javax.swing.JButton jButton7;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton btnAddCustomer;
+    private javax.swing.JButton btnHome;
+    private javax.swing.JButton btnListOrders;
+    private javax.swing.JButton btnManageProduct;
+    private javax.swing.JButton btnSearchCustomer;
+    private javax.swing.JButton btnViewProduct;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JSplitPane jSplitPane1;
+    private javax.swing.JLabel lblWelcome;
+    private javax.swing.JPanel menuBar;
+    private javax.swing.JSplitPane splitPane;
+    private javax.swing.JPanel workArea;
     // End of variables declaration//GEN-END:variables
 }
